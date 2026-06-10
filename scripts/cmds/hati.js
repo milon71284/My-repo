bba029e1516579a21dae5c84ef9d11d2c3d5ced8
Milon7-cell <goatbot72@gmail.com>
2026-06-10 15:12:09 +0600

    Update and rename elephant.js to hati.js

--- scripts/cmds/hati.js
+++ scripts/cmds/hati.js
@@ -9,7 +9,7 @@ const baseApiUrl = async () => {
 
 module.exports = {
         config: {
-                name: "elephant",
+                name: "hati",
                 version: "1.7",
                 author: "MahMUD",
                 role: 0,
