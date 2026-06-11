638e380827db11523da8ea06f61199444219c355
Milon7-cell <goatbot72@gmail.com>
2026-06-11 22:37:09 +0600

    Update help.js

--- scripts/cmds/help.js
+++ scripts/cmds/help.js
@@ -63,7 +63,7 @@ module.exports = {
                                        `Type ${prefix}help <cmd> to see details.`;
 
                         msg += `\n\n⭔ Total Commands: ${totalCommands}\n⭔ ${helpHint}\n`;
-                        msg += `\n╭─✦ ADMIN: MahMUD 彡\n├‣ WHATSAPP\n╰‣ 01836298139`;
+                        msg += `\n╭─✦ ADMIN: Milon 彡\n├‣ massanger\n╰‣ m.me/100081225144815`;
 
                         try {
                                 const hh = await message.reply({ body: msg });
