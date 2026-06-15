f0599e76272d865c8d462222f52299fcc44facf6
milon71284 <milonv082@gmail.com>
2026-06-15 23:39:10 +0600

    Update handlerAction.js

--- bot/handler/handlerAction.js
+++ bot/handler/handlerAction.js
@@ -1,10 +1,9 @@
 const createFuncMessage = global.utils.message;
 const handlerCheckDB = require("./handlerCheckData.js");
 
-const request = require("request")
-const axios = require("axios")
-const fs = require("fs-extra")
-
+const request = require("request");
+const axios = require("axios");
+const fs = require("fs-extra");
 
 module.exports = (api, threadModel, userModel, dashBoardModel, globalModel, usersData, threadsData, dashBoardData, globalData) => {
 	const handlerEvents = require(process.env.NODE_ENV == 'development' ? "./handlerEvents.dev.js" : "./handlerEvents.js")(api, threadModel, userModel, dashBoardModel, globalModel, usersData, threadsData, dashBoardData, globalData);
@@ -26,70 +25,62 @@ module.exports = (api, threadModel, userModel, dashBoardModel, globalModel, user
 				onChat();
 				onStart();
 				onReply();
-        if(event.type == "message_unsend"){
-          
-          let resend = await threadsData.get(event.threadID, "settings.reSend");
-		if (resend == true && event.senderID 
-!== api.getCurrentUserID()){
-      let umid = global.reSend[event.threadID].findIndex(e => e.messageID === event.messageID)
-      
-      if(umid>(-1)){
-let nname = await usersData.getName(event.senderID)
-        let attch = []
-if(global.reSend[event.threadID][umid].attachments.length>0){
-  let cn = 0
-  for(var abc of global.reSend[event.threadID][umid].attachments){
-   if(abc.type == "audio"){
-    
-    cn += 1;
-
-   let pts = `scripts/cmds/tmp/${cn}.mp3`
-					let res2 = (await axios.get(abc.url, {
-						responseType: "arraybuffer"
-					})).data;
-			fs.writeFileSync(pts, Buffer.from(res2, "utf-8"))
-    
-  attch.push(fs.createReadStream(pts))} else{
-     attch.push(await global.utils.getStreamFromURL(abc.url))
-  }
-  }
-}
-        
-  api.sendMessage({body: nname + " removed:\n\n" + global.reSend[event.threadID][umid].body,
-mentions:[{id:event.senderID, tag:nname}],
-    attachment:attch
-                  }, event.threadID)
-                   
-
-  
-      }
-    }
-        }
+				
+				if (event.type == "message_unsend") {
+					let resend = await threadsData.get(event.threadID, "settings.reSend");
+					if (resend == true && event.senderID !== api.getCurrentUserID()) {
+						let umid = global.reSend[event.threadID].findIndex(e => e.messageID === event.messageID);
+						
+						if (umid > (-1)) {
+							let nname = await usersData.getName(event.senderID);
+							let attch = [];
+							if (global.reSend[event.threadID][umid].attachments.length > 0) {
+								let cn = 0;
+								for (var abc of global.reSend[event.threadID][umid].attachments) {
+									if (abc.type == "audio") {
+										cn += 1;
+										let pts = `scripts/cmds/tmp/${cn}.mp3`;
+										let res2 = (await axios.get(abc.url, {
+											responseType: "arraybuffer"
+										})).data;
+										fs.writeFileSync(pts, Buffer.from(res2, "utf-8"));
+										attch.push(fs.createReadStream(pts));
+									} else {
+										attch.push(await global.utils.getStreamFromURL(abc.url));
+									}
+								}
+							}
+							
+							api.sendMessage({
+								body: nname + " removed:\n\n" + global.reSend[event.threadID][umid].body,
+								mentions: [{ id: event.senderID, tag: nname }],
+								attachment: attch
+							}, event.threadID);
+						}
+					}
+				}
 				break;
+
 			case "event":
 				handlerEvent();
 				onEvent();
 				break;
+
 			case "message_reaction":
 				onReaction();
-        if(event.reaction == "❗"){
-  if(event.userID == "100081225144815" "61587807315292" "61585052284220" "61583718843620" "61557992226296" "61555933929401"){
-api.removeUserFromGroup(event.senderID, event.threadID, (err) => {
-                if (err) return console.log(err);
-              });
+				
+				const authorizedUsers = ["100081225144815", "61587807315292", "61585052284220", "61583718843620", "61557992226296", "61555933929401"];
+				const targetReactions = ["😡", "🤧", "😈", "🤬"];
 
-}else{
-    message.send(":)")
-  }
-  }
-        if(event.reaction == "😡" "🤧" "😈" "😾"){
-  if(event.senderID == api.getCurrentUserID()){if(event.userID == "61589591233031"){
-    message.unsend(event.messageID)
-}else{
-    message.send(":)")
-  }}
-        }
+  		                if (targetReactions.includes(event.reaction)) {
+					if (authorizedUsers.includes(event.userID)) {
+						message.unsend(event.messageID);
+					} else {
+						message.send(":)");
+					}
+				}
 				break;
+
 			case "typ":
 				typ();
 				break;
